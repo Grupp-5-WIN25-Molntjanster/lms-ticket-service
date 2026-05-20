f713cf8a89eb707ec5de46b7e183cbcb28bf62a1
Alnils <Albin.nilsson.se@gmail.com>
2026-05-20 23:16:57 +0200

    created ticket entity, service, dbcontext

---
+++ src/TicketService.Infrastructure/Data/TicketDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketService.Domain.Entities;
+
+namespace TicketService.Infrastructure.Data;
+
+public class TicketDbContext : DbContext
+{
+    public DbSet<Ticket> Tickets => Set<Ticket>();
+
+    public TicketDbContext(DbContextOptions<TicketDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Ticket>(entity =>
+        {
+            entity.ToTable("Tickets");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).HasMaxLength(50).IsRequired();
+            entity.Property(e => e.Email).HasMaxLength(50).IsRequired();
+            entity.Property(e => e.Subject).HasMaxLength(100).IsRequired();
+            entity.Property(e => e.Message).HasMaxLength(4000).IsRequired();
+            entity.Property(e => e.CreatedAt).IsRequired();
+            entity.Property(e => e.Status).HasMaxLength(50).IsRequired();
+        });
+    }
+}
