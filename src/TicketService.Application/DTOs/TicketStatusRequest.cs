b83d7f48dd1fbab20fb8c8a661b553956c79e1b0
Alnils <Albin.nilsson.se@gmail.com>
2026-05-21 16:36:49 +0200

    created ticketStatusRequest

---
+++ src/TicketService.Application/DTOs/TicketStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TicketService.Application.DTOs;
+
+public record TicketStatusRequest
+(
+    [Required(ErrorMessage = "Status is required")]
+    string Status
+);
